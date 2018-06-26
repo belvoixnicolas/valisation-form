a33bfed2dbb527666b746f34a0a96af0ba854829
belvoixnicolas <belvoixnicolas1997@gmail.com>
2018-06-26 14:01:22 +0200

    t

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,35 @@ function validateEmail($email){
 		return true;
 	}
 }
+
+function checkForm() {
+  var input = document.querySelectorAll('input[type=text]');
+  var mail = document.querySelectorAll('input[type=email]');
+
+  ////// VERIF TOUT LES INPUT TEXT \\\\\\
+  for (var i = 0; i < input.length; i++) {
+    if ($(input[i]).val() == "") {
+      $(input[i]).addClass('error');
+    }else {
+      $(input[i]).removeClass('error');
+    }
+  }
+
+  ////// VERIF TOUT LES INPUT EMAIL \\\\\\
+  for (var i = 0; i < mail.length; i++) {
+    var verif = validateEmail($(mail[i]).val());
+
+    if (verif == false || $(mail[i]).val() == "") {
+      $(mail[i]).addClass('error');
+    }else {
+      $(mail[i]).removeClass('error');
+    }
+  }
+
+  ////// VERIF SELECT \\\\\\
+  if ($('select').val() == 'Choose') {
+    $('select').addClass('error');
+  }else {
+    $('select').removeClass('error');
+  }
+}
